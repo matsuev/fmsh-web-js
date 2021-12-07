ba047eb871d0fb7fe6ba278d9f4339e6e7fee139
Alex Matsuev <alex.matsuev@gmail.com>
2021-12-07 15:09:11 +0700

    Update Arcanoid

--- arcanoid/script.js
+++ arcanoid/script.js
@@ -2,25 +2,29 @@ const gameBoard = document.getElementById("gameBoard")
 const racket = document.getElementById("racket")
 const ball = document.getElementById("ball")
 
-var racketObj = new(Object) 
+var gameOver = false
 
-racketObj.X = racket.offsetLeft
-racketObj.L = racket.clientWidth
-racketObj.dX = 10
+var racketObj = {
+   X: racket.offsetLeft,
+   L: racket.clientWidth,
+   dX: 10,
+}
 
-var ballObj = new(Object)
-ballObj.X = ball.offsetLeft 
-ballObj.Y = ball.offsetTop
-ballObj.dX = 1
-ballObj.dY = 1
+var ballObj = {
+   X: ball.offsetLeft,
+   Y: ball.offsetTop,
+   R: 20,
+   dX: 3,
+   dY: -3,
+}
 
 
 function onArrowKeyDown(ev) {
-   if (ev.code == "ArrowRight") {
+   if ((ev.code == "ArrowRight") && (racketObj.X + racketObj.L + racketObj.dX < gameBoard.clientWidth)) {
       racketObj.X += racketObj.dX
    }
 
-   if (ev.code == "ArrowLeft") {
+   if ((ev.code == "ArrowLeft") && (racketObj.X > racketObj.dX)) {
       racketObj.X -= racketObj.dX
    }
 
@@ -30,11 +34,34 @@ function onArrowKeyDown(ev) {
 document.addEventListener("keydown", onArrowKeyDown)
 
 function moveBall() {
+   if (ballObj.X + ballObj.dX < 0) {
+      ballObj.dX *= -1
+   }
+
+   if (ballObj.X + ballObj.dX + 2*ballObj.R > gameBoard.clientWidth) {
+      ballObj.dX *= -1
+   }
+
+   if (ballObj.Y + ballObj.dY < 0) {
+      ballObj.dY *= -1
+   }
+
+   if (ballObj.Y + ballObj.dY + 2*ballObj.R > gameBoard.clientHeight) {
+      gameOver = true
+      alert("Game over!")
+   }
+
+
    ballObj.X += ballObj.dX
    ballObj.Y += ballObj.dY
+
    ball.style.left = ballObj.X + "px"
    ball.style.top = ballObj.Y + "px"
-   window.requestAnimationFrame(moveBall)
+
+   if (!gameOver) {
+      window.requestAnimationFrame(moveBall)
+   }
+   
 }
 
 window.requestAnimationFrame(moveBall)
